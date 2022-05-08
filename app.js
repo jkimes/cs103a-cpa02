1901e6ea43f738226c3e5bf8667d2698706007d5
Jalon <jkimes@brandeis.edu>
2022-05-07 20:40:56 -0400

    removed node modules

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const Movie = require("./Models/Movie")
 const Item = require("./models/Item")
-//const mongodb_URI = "mongodb+srv://Jalon:a@cluster0.xsiln.mongodb.net/Movies"
-const mongodb_URI = process.env.mongodb_URI
+const mongodb_URI = "mongodb+srv://Jalon:a@cluster0.xsiln.mongodb.net/Movies"
+//const mongodb_URI = process.env.mongodb_URI
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -204,9 +204,9 @@ app.get('/Search/show/:title',
     }
 )
 
-const port = process.env.PORT || "5000";
-console.log('connecting on port '+port)
-app.set("port", port);
+// const port = process.env.PORT || "5000";
+// console.log('connecting on port '+port)
+// app.set("port", port);
 
 
 // app.listen(process.env.PORT || 3000, function(){
@@ -214,64 +214,64 @@ app.set("port", port);
 //   });
 
 
-// // *********************************************************** //
-// //  Starting up the server!
-// // *********************************************************** //
-// //Here we set the port to use between 1024 and 65535  (2^16-1)
-// const port = "7000";
-// app.set("port", port);
+// *********************************************************** //
+//  Starting up the server!
+// *********************************************************** //
+//Here we set the port to use between 1024 and 65535  (2^16-1)
+const port = "7000";
+app.set("port", port);
 
-// // and now we startup the server listening on that port
-// const http = require("http");
-// const server = http.createServer(app);
+// and now we startup the server listening on that port
+const http = require("http");
+const server = http.createServer(app);
 
-// server.listen(port);
+server.listen(port);
 
-// function onListening() {
-//     var addr = server.address();
-//     var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-//     debug("Listening on " + bind);
-// }
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+    debug("Listening on " + bind);
+}
 
-// function onError(error) {
-//     if (error.syscall !== "listen") {
-//         throw error;
-//     }
+function onError(error) {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
 
-//     var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+    var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
-//     // handle specific listen errors with friendly messages
-//     switch (error.code) {
-//         case "EACCES":
-//             console.error(bind + " requires elevated privileges");
-//             process.exit(1);
-//             break;
-//         case "EADDRINUSE":
-//             console.error(bind + " is already in use");
-//             process.exit(1);
-//             break;
-//         default:
-//             throw error;
-//     }
-// }
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case "EACCES":
+            console.error(bind + " requires elevated privileges");
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(bind + " is already in use");
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
 
-// server.on("error", onError);
+server.on("error", onError);
 
-// server.on("listening", onListening);
+server.on("listening", onListening);
 
-// module.exports = app;
+module.exports = app;
 
 
-// app.get('/upsertDB',
-//     async(req, res, next) => {
-//         //await Course.deleteMany({})
-//         for (m in movies) {
-//             // console.log(m.title)
-//             const { title, year, genres, rating, poster } = movies;
-//             // console.log("Upsert " + m)
-//             await Movie.findOneAndUpdate({ title, year, genres, rating, poster  }, m, { upsert: true })
-//         }
-//         const num = await Movie.find({}).count();
-//         res.send("data uploaded: " + num)
-//     }
-// )
+app.get('/upsertDB',
+    async(req, res, next) => {
+        //await Course.deleteMany({})
+        for (m in movies) {
+            // console.log(m.title)
+            const { title, year, genres, rating, poster } = movies;
+            // console.log("Upsert " + m)
+            await Movie.findOneAndUpdate({ title, year, genres, rating, poster  }, m, { upsert: true })
+        }
+        const num = await Movie.find({}).count();
+        res.send("data uploaded: " + num)
+    }
+)
